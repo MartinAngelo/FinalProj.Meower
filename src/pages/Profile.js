77ba625780905ad40cd5fc08172926500d262406
MartinAngelo <gelomartin163@gmail.com>
2021-05-16 21:51:05 +0800

    add dp

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from "../Components/nav"
+import dp from "../pic/profile.png"
 
 import firebase from "../utils/firebase";
 
@@ -8,27 +9,31 @@ export default function Profile() {
     const [state, setState] = useState({
         user: null
     })
-    
+
     useEffect(() => {
         var signedUser = firebase.auth().currentUser;
-    
+
         if (signedUser) {
             // User is signed in.
-            setState({user : signedUser})
+            setState({ user: signedUser })
         } else {
             // No user is signed in.
             setState(null)
         }
     }, [])
-    
+
 
 
     return (
         <div>
             <Nav />
+            <div className="dps">
+                <img src={dp} alt="Logo" />
+            
             <h1 class="welcome">Welcome   {state.user ? state.user.email : "null"}</h1>
             <p class="tag">Account Details</p>
-            
+            </div>
+
         </div>
     )
 }
