4eaa14623d18533c253bc616a406898295b7b32e
MartinAngelo <gelomartin163@gmail.com>
2021-05-20 12:27:51 +0800

    add

--- src/pages/login.js
+++ src/pages/login.js
@@ -56,6 +56,8 @@ export default function Login() {
     <div className="container">
       <div className="left">
         <div className="header">
+        <h1 className="animation a1">MovieFlix</h1>
+        <br />
           <h2 className="animation a1">Welcome Back</h2>
           <h4 className="animation a2">Log in to your account using email and password</h4>
         </div>
